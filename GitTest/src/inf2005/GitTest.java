1f79d2215830f8a5109fe6bf1da791f90d148280
Patrick Boudreau <patrick@lpcmedia.com>
2011-09-16 18:45:17 -0400

    Created with appropriate Filename

---
+++ GitTest/src/inf2005/GitTest.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inf2005;
+
+/**
+ *
+ * @author patrick
+ */
+public class GitTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
